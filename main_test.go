9fe6945f8f9a6187e75b24064dca9c65dbef0e3a
sacry-dyn <matthias.nitsche@dynport.de>
2015-09-21 17:18:17 +0200

    added sample for push config as well

--- main_test.go
+++ main_test.go
@@ -27,3 +27,25 @@ phraseapp:
 		t.Errorf("Expected FileFormat of first target to be %s and not %s", "strings", targetParams.FileFormat)
 	}
 }
+
+func TestPushConfig(t *testing.T) {
+	pushConfig := `
+phraseapp:
+  push:
+    sources:
+      -
+        file: ./locales/file.yml
+        params:
+          file_format: strings
+`
+	config := &PushConfig{}
+	err := yaml.Unmarshal([]byte(pushConfig), &config)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	sourceParams := config.Phraseapp.Push.Sources[0].Params
+	if sourceParams.FileFormat != "strings" {
+		t.Errorf("Expected FileFormat of first target to be %s and not %s", "strings", sourceParams.FileFormat)
+	}
+}
