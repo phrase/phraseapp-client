cb3f94624b9e54ddee7660ab07d47aa7b3bad62d
Tobias Schwab <tobias.schwab@dynport.de>
2017-01-23 15:02:03 +0100

    print backtrace on panics

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"runtime"
 
 	bserrors "github.com/bugsnag/bugsnag-go/errors"
 	"github.com/dynport/dgtk/cli"
@@ -19,13 +21,25 @@ func Run() {
 	var cfg *phraseapp.Config
 	defer func() {
 		if recovered := recover(); recovered != nil {
-			if recovered, ok := recovered.(error); ok && PHRASEAPP_CLIENT_VERSION != "DEV" {
-				reportError(bserrors.New(recovered, 3), cfg)
+			if rc, ok := recovered.(string); ok {
+				l := log.New(os.Stderr, "", 0)
+				stack := make([]uintptr, bserrors.MaxStackDepth)
+				length := runtime.Callers(0, stack[:])
+				l.Printf("PANIC: %s", rc)
+				for i := 0; i < length; i++ {
+					f := runtime.FuncForPC(stack[i])
+					file, line := f.FileLine(stack[i])
+					l.Printf("%s:%d: %s", file, line, f.Name())
+				}
 			}
 			printError(fmt.Errorf("This should not have happened: %s - Contact support: %s", recovered, phraseAppSupport))
 			os.Exit(1)
 		}
 	}()
+
+	if os.Getenv("DO_PANIC") == "true" {
+		panic("testing a panic")
+	}
 
 	phraseapp.ClientVersion = PHRASEAPP_CLIENT_VERSION
 	CheckForUpdate(os.Stderr)
