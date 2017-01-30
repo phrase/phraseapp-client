1e95d426d74baf8dc93b2258aa718bde37b7d977
alexanderwilling <alexander.willing@phraseapp.com>
2017-01-30 11:27:18 +0100

    don't check for error type; print stack in debug mode (#78)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	bserrors "github.com/bugsnag/bugsnag-go/errors"
 	"github.com/dynport/dgtk/cli"
@@ -19,8 +20,11 @@ func Run() {
 	var cfg *phraseapp.Config
 	defer func() {
 		if recovered := recover(); recovered != nil {
-			if recovered, ok := recovered.(error); ok && PHRASEAPP_CLIENT_VERSION != "DEV" {
-				reportError(bserrors.New(recovered, 3), cfg)
+			if PHRASEAPP_CLIENT_VERSION != "DEV" {
+				reportError(bserrors.New(recovered, 1), cfg)
+			}
+			if Debug {
+				fmt.Fprintf(os.Stderr, "%v\n%s", recovered, debug.Stack())
 			}
 			printError(fmt.Errorf("This should not have happened: %s - Contact support: %s", recovered, phraseAppSupport))
 			os.Exit(1)
