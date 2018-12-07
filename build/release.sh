76bcabf24c930be7d3c64294af0fb98642292f6c
Sönke Behrendt <soenke.behrendt@gmail.com>
2018-12-07 14:39:45 +0100

    Try machine executor

--- build/release.sh
+++ build/release.sh
@@ -3,28 +3,28 @@
 set -eo pipefail
 
 # validate that master is checked out and head points to origin/master
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [ $BRANCH != 'master' ]; then
-    echo "Not on master branch. Aborting"
-    exit 1
-fi
+# BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# if [ $BRANCH != 'master' ]; then
+#     echo "Not on master branch. Aborting"
+#     exit 1
+# fi
 
- HEADHASH=$(git rev-parse HEAD)
- UPSTREAMHASH=$(git rev-parse master@{upstream})
+#  HEADHASH=$(git rev-parse HEAD)
+#  UPSTREAMHASH=$(git rev-parse master@{upstream})
 
- if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
-   echo "Not up to date with origin/master. Aborting"
-   exit 1
- fi
+#  if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
+#    echo "Not up to date with origin/master. Aborting"
+#    exit 1
+#  fi
 
-# Validate current tag against version
-current_tag=$(git describe --tags --abbrev=0)
-client_version=$(cat .version)
+# # Validate current tag against version
+# current_tag=$(git describe --tags --abbrev=0)
+# client_version=$(cat .version)
 
-if [ $current_tag != $client_version ];then
-    echo "client version '${client_version}' and tag '${current_tag}' do not match"
-    exit 1
-fi
+# if [ $current_tag != $client_version ];then
+#     echo "client version '${client_version}' and tag '${current_tag}' do not match"
+#     exit 1
+# fi
 
 # Build client
 ./build/build.sh
