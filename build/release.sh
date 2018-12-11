d7960e744f58c28545ac6349ddae9c72c0c309da
Sönke Behrendt <soenke.behrendt@gmail.com>
2018-12-11 16:47:31 +0100

    CircleCI: Use machine executor to fix directory mount

--- build/release.sh
+++ build/release.sh
@@ -12,18 +12,15 @@ fi
  HEADHASH=$(git rev-parse HEAD)
  UPSTREAMHASH=$(git rev-parse master@{upstream})
 
- if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
-   echo "Not up to date with origin/master. Aborting"
-   exit 1
- fi
-
-# Validate current tag against version
-current_tag=$(git describe --tags --abbrev=0)
-client_version=$(cat .version)
-
-if [ $current_tag != $client_version ];then
-    echo "client version '${client_version}' and tag '${current_tag}' do not match"
+if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
+    echo "Not up to date with origin/master. Aborting"
     exit 1
+fi
+
+client_version=$(cat .version)
+if git rev-parse $client_version >/dev/null 2>&1; then
+    echo "Release already tagged"
+    exit 0
 fi
 
 # Build client
