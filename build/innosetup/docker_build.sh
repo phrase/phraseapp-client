abd08a5c176230d3b0c94d54e59aa0cefa879098
Sönke Behrendt <soenke.behrendt@gmail.com>
2019-05-21 11:16:22 +0200

    Fix path for innosetup 6

--- build/innosetup/docker_build.sh
+++ build/innosetup/docker_build.sh
@@ -4,8 +4,8 @@ set -e
 
 cd /client
 cp LICENSE build/innosetup/
-wine '/root/.wine/drive_c/Program Files/Inno Setup 5/ISCC.exe' build/innosetup/phraseapp-client.iss
-wine '/root/.wine/drive_c/Program Files/Inno Setup 5/ISCC.exe' build/innosetup/phraseapp-client-386.iss
+wine '/root/.wine/drive_c/Program Files/Inno Setup 6/ISCC.exe' build/innosetup/phraseapp-client.iss
+wine '/root/.wine/drive_c/Program Files/Inno Setup 6/ISCC.exe' build/innosetup/phraseapp-client-386.iss
 mv build/innosetup/Output/phraseapp_setup_386.exe dist/phraseapp_windows_setup_386.exe
 mv build/innosetup/Output/phraseapp_setup.exe dist/phraseapp_windows_setup.exe
 rm build/innosetup/LICENSE
