633cd861a49cd56dbe0a53d0bf3a6bffa460c3dc
Sönke Behrendt <soenke.behrendt@gmail.com>
2019-05-27 11:16:50 +0200

    Update to 1.14.2

--- build/config.sh
+++ build/config.sh
@@ -6,7 +6,7 @@ export BUILD_DIR=$(realpath $(dirname $0)/..)
 pushd $BUILD_DIR > /dev/null
 export GOVERSION=${GOVERSION:-1.11}
 export REVISION=${GIT_COMMIT:-$(git rev-parse HEAD)}
-export LIBRARY_REVISION="700406d0fbd2d105712a20170097b7cb871acfa7"
+export LIBRARY_REVISION="3f4dacd4d38a89e715d8c5f52f7cfbf776728a2f"
 export VERSION=$(cat ${wd}/.version)
 export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
 export LAST_CHANGE=$(git log -1 --format=%cd)
