f84818af7c9847952b0f2479d9a4b5aa8838751b
Sönke Behrendt <soenke.behrendt@gmail.com>
2019-08-09 15:23:24 +0200

    Auto extract library revision again

--- build/config.sh
+++ build/config.sh
@@ -6,7 +6,7 @@ export BUILD_DIR=$(realpath $(dirname $0)/..)
 pushd $BUILD_DIR > /dev/null
 export GOVERSION=${GOVERSION:-1.11}
 export REVISION=${GIT_COMMIT:-$(git rev-parse HEAD)}
-export LIBRARY_REVISION="3f4dacd4d38a89e715d8c5f52f7cfbf776728a2f"
+export LIBRARY_REVISION=$(cat Gopkg.lock | grep github.com/phrase/phraseapp-go -A 2 | tail -n 1 | cut -d '"' -f 2)
 export VERSION=$(cat ${wd}/.version)
 export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
 export LAST_CHANGE=$(git log -1 --format=%cd)
